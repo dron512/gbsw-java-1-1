b923f950bd4ca848290fb1a562ea8b21987dcf5e
mh <parkmyounghoi@dgsw.hs.kr>
2026-09-04 11:21:50 +0900

    no message

---
+++ ex07/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[30];
+        System.out.println(arr.length);
+
+        String a = "abcd";
+        System.out.println(a.length());
+    }
+}
