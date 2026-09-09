66ee3583af250e5c6476ed3dab4eb31a2adf083a
mh <parkmyounghoi@dgsw.hs.kr>
2026-09-09 11:02:28 +0900

    no message

--- ex10/src/Ex03.java
+++ ex10/src/Ex03.java
@@ -1,5 +1,4 @@
 public class Ex03 {
-
     public static void main(String[] args) {
         // Product 클래스 생성하고
         // 필드 name, price , desc
@@ -11,8 +10,8 @@ public class Ex03 {
         // 3 = [{"CCC",6000,"엄청비싸네"}]
         // 4 = [{"DDD",10000,"음"}]
 
-        // 상품의 평균값을 구하고
-        // 상품의 평균값 이상의 개수를 구하시오...
-
+        // 1. 배열의 내용을 출력
+        // 2. 상품의 평균값을 출력
+        // 3. 상품의 평균값 이상의 개수 출력
     }
 }
